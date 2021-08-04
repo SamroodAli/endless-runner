6d39ee3c0f72d5944074cf46958458f10fe70b32
samroodAli <devsamrood@gmail.com>
2021-08-04 14:38:57 +0530

    Refactor:Import assets platform and dude and load them to Game scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,11 +1,14 @@
 import Phaser from "phaser";
-import platform from "../assets/ground_grass.png";
+import platform from "../assets/platform.png";
+import dude from "./assets/dude.png";
+
 class Game extends Phaser.Scene {
   constructor() {
     super("game");
   }
   preload() {
     this.load.image("platform", platform);
+    this.load.spritesheet("dude", dude, { frameWidth: 32, frameHeight: 48 });
   }
   create() {
     this.add.image(200, 320, "platform").setScaleX(2).setScaleY(0.5);

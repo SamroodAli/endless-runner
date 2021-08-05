38a88b070b6e8b3a6be3af7ca356dd309487e869
samroodAli <devsamrood@gmail.com>
2021-08-05 18:08:46 +0530

    Update:Write next platform with pooling and grouping with code for jump

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import "./style.scss";
 import Phaser from "phaser";
 import { gameConfig } from "./gameOptions.js";
 
-export default new Phaser.Game(gameConfig);
+new Phaser.Game(gameConfig);
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -13,18 +13,19 @@ class Game extends Phaser.Scene {
     this.load.spritesheet("dude", dude, { frameWidth: 32, frameHeight: 48 });
   }
   create() {
+    console.log("game started baby");
     this.platformGroup = this.add.group({
       // once a platform is removed, it's added to the pool
-      removeCallback: function (platform) {
-        platform.scene.platformPool.add(platform);
+      removeCallback: (platform) => {
+        this.platformPool.add(platform);
       },
     });
 
     // pool
     this.platformPool = this.add.group({
       // once a platform is removed from the pool, it's added to the active platforms group
-      removeCallback: function (platform) {
-        platform.scene.platformGroup.add(platform);
+      removeCallback: (platform) => {
+        this.platformGroup.add(platform);
       },
     });
     this.playerJumps = 0;
@@ -53,9 +54,48 @@ class Game extends Phaser.Scene {
       );
     }
     this.player.x = gameOptions.playerStartPosition;
+
+    // recycling platforms
+    let minDistance = gameConfig.width; //screen Width
+    this.platformGroup.getChildren().forEach((platform) => {
+      let platformDistance =
+        gameConfig.width - (platform.x + platform.displayWidth / 2); //1050
+
+      console.log(`
+        gameConfig.width:       ${gameConfig.width}
+        platform.x              ${platform.x}
+        platform.displayWidth/2 ${platform.displayWidth / 2}
+        ___________________________________________________
+        platform Distance =     ${platformDistance}
+        `);
+      console.log(
+        `minDistance: ${minDistance}, platformDistance: ${platformDistance}`
+      );
+      minDistance = Math.min(minDistance, platformDistance); // 1050
+      if (platform.x < -platform.displayWidth / 2) {
+        this.platformGroup.killAndHide(platform);
+        this.platformGroup.remove(platform);
+      }
+    }, this);
+    console.log(minDistance, this.nextPlatformDistance);
+    // adding new platforms
+    if (minDistance > this.nextPlatformDistance) {
+      var nextPlatformWidth = Phaser.Math.Between(
+        gameOptions.platformSizeRange[0],
+        gameOptions.platformSizeRange[1]
+      );
+      this.addPlatform(
+        nextPlatformWidth,
+        gameConfig.width + nextPlatformWidth / 2
+      );
+    }
+    if (this.player.body.touching.down) {
+      this.playerJumps = 0;
+    }
   }
 
   addPlatform(platformWidth, posX) {
+    //add from the pool if available or else create a platform and add it to the group
     let platform;
     if (this.platformPool.getLength()) {
       platform = this.platformPool.getFirst();
@@ -64,7 +104,7 @@ class Game extends Phaser.Scene {
       platform.visible = true;
       this.platformPool.remove(platform);
     } else {
-      platform = this.physics.add.sprite(
+      platform = this.physics.add.image(
         posX,
         gameConfig.height * 0.7,
         "platform"
@@ -80,13 +120,7 @@ class Game extends Phaser.Scene {
     );
   }
   jump() {
-    if (
-      this.player.body.touching.down ||
-      (this.playerJumps > 0 && this.playerJumps < gameOptions.jumps)
-    ) {
-      if (this.player.body.touching.down) {
-        this.playerJumps = 0;
-      }
+    if (this.playerJumps < gameOptions.jumps) {
       this.player.setVelocityY(gameOptions.jumpForce * -1);
       this.playerJumps++;
     }
